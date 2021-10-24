3ac5ea53918389667754067d2a5b6798492a7f3a
Clarence Bakirtzidis <clarenceb@gmail.com>
2021-10-24 17:49:55 +1100

    added app insights key

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  instrumentationKey: '62ba1e2e-54bc-4b12-b9d6-6fa99119cfb5'
+  /* ...Other Configuration Options... */
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
+
 Vue.config.productionTip = false
 
 new Vue({
